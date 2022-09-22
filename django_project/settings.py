a084bdd3937333608ce22a5b1555d9e8a9e01372
Savakar Rohan <rohansavakar@gmail.com>
2022-09-22 19:40:52 +0530

    blank templates and settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     # Local apps
     # authentication app
     'accounts.apps.AccountsConfig',
+    # pages default layout app
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
