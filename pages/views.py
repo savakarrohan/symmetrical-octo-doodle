54735a842894423b6524a0fe1c900085e20ba1d5
Savakar Rohan <rohansavakar@gmail.com>
2022-09-22 19:50:38 +0530

    simple homepage added

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views.generic import TemplateView 
+class HomePageView(TemplateView):
+    """Currently basic"""
+    template_name = "home.html"
