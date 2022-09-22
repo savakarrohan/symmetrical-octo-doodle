e177ab718d1324d65f1b1343ffa8fc9b1b0ebdd7
Savakar Rohan <rohansavakar@gmail.com>
2022-09-22 19:57:23 +0530

    test success

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
-
-# Create your tests here.
+from urllib import response
+from django.test import SimpleTestCase
+from django.urls import reverse
+# Classes for the Homepage with layout home.html
+class HomepageTests(SimpleTestCase):
+    """ tests for the Homepage"""
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,200)
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code,200)
+        
