49de2e603c322d9059d47c6f484e8e9acee7d431
Savakar Rohan <rohansavakar@gmail.com>
2022-09-22 22:40:39 +0530

    static direction location

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,8 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
+# STATIC_URL is the directory from where they are served
+STATIC_URL = '/static/'
+# Default directory to look for while searching for static files.
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
