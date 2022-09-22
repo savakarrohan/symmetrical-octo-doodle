08866e822c3d358a35b9b948ae0cf69eea1b72cc
Savakar Rohan <rohansavakar@gmail.com>
2022-09-22 20:09:27 +0530

    finished pages with tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,26 +1,30 @@
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse,resolve
+
+from pages.views import HomePageView
 # Classes for the Homepage with layout home.html
 class HomepageTests(SimpleTestCase):
     """ tests for the Homepage"""
+    def setUp(self):
+        """Set up of self for tests"""
+        url = reverse("home")
+        self.response = self.client.get(url)
     def test_url_exists_at_correct_location(self):
         """url location"""
-        response = self.client.get("/")
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(self.response.status_code,200)
     def test_homepage_url_name(self):
         """ url name"""
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(self.response.status_code,200)
     def test_homepage_template(self):
         """ template"""
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
     def test_homepage_contains_correct_html(self):
         """ text at link"""
-        response = self.client.get("/")
-        self.assertContains(response, "home page")
+        self.assertContains(self.response, "home page")
     def test_homepage_does_not_contain_incorrect_html(self):
         """ test for not required text"""
-        response = self.client.get("/")
-        self.assertNotContains(response, "Hi there! I should not be on the page.")
-    
+        self.assertNotContains(self.response, "Hi there! I should not be on the page.")
+    def test_homepage_url_resolves_homepageview(self):
+        """ check if the names are corect at both links"""
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
