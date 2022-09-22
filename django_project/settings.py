5e666101357ac030a5e6c3d95d565a378621096f
Savakar Rohan <rohansavakar@gmail.com>
2022-09-23 00:09:21 +0530

    crispy forms

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # thirdparty apps
+    'crispy_forms',
+    'crispy_bootstrap5',
     # Local apps
     # authentication app
     'accounts.apps.AccountsConfig',
